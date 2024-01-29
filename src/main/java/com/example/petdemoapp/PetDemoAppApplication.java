29ac735338380eba478e39f0075afe5e0b158bbc
DenRyzhov <126025526+DenRyzhov@users.noreply.github.com>
2024-01-29 19:47:18 +0300

    Initial commit

---
+++ src/main/java/com/example/petdemoapp/PetDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.petdemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetDemoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetDemoAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/petdemoapp/PetDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petdemoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetDemoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
